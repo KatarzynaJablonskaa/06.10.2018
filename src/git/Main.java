6e2944016e1a74a846056aa5236aceb4a40d5736
Katarzyna Jabłońska <katarzyna.k.jablonska@gmail.com>
2018-10-06 13:03:58 +0200

    change some piece of code

--- src/git/Main.java
+++ src/git/Main.java
@@ -3,7 +3,8 @@ package git;
 public class Main {
 
     public static void main(String... args) {
-        System.out.println(args.length +getMsg());
+        System.out.println(args.length);
+        System.out.println(getMsg());
     }
 
     private static String getMsg() {
